fac4fe5285e7c2445b275afcd40485368d013ef1
Alexander Scherbakov <ashcherbakov@fevo.com>
2020-06-13 11:44:28 +0200

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+require('dotenv').config();
+import express from 'express';
+import authRouter from './auth';
+import appRouter from './routing';
+import mongoConnect from './db/v1';
+
+const app = express();
+
+// Simple check if body contains well defined content
+app.use((req, res, next) =>
+  express.json()(req, res, (err) => (err ? res.sendStatus(400) : next()))
+);
+
+app.use(authRouter);
+app.use(appRouter);
+app.all('*', (_, res) => {
+  res.status(404).json({
+    message: 'Nothing here',
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Running at localhost:${process.env.PORT}`);
+});
+mongoConnect();
